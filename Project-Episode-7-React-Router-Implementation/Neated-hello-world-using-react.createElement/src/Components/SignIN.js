5e6c5636654e34c50b942ad9ce26f50da1be9be2
harsh9534 <harsh9534gupta@gmail.com>
2024-08-30 12:31:44 +0530

    typewriter-effect

--- Project-Episode-7-React-Router-Implementation/Neated-hello-world-using-react.createElement/src/Components/SignIN.js
+++ Project-Episode-7-React-Router-Implementation/Neated-hello-world-using-react.createElement/src/Components/SignIN.js
@@ -1,25 +1,37 @@
 import React from "react";
 import logo from "../utils/logo.png";
+import Typewriter from "typewriter-effect";
 
 const SignIN = () => {
   return (
     <div className="flex  row">
       <div className="bg-white h-screen w-1/2 -mt-2    ">
         <div className="flex flex-col ">
-          <img src={logo} className="h-[270] w-[450] m-auto "></img>
-          <span
-            className="font-inter font-thin text-6xl w-auto mt-7 ml-32 "
-            style={{ width: "max-content" }}
-          >
-            Gupta Eats,
-          </span>
-          <br />
-          <span
-            className="font-inter font-thin text-6xl inline-block mt-2 ml-36"
-            style={{ width: "max-content" }}
-          >
-            Cravings Satisfied.
-          </span>
+          <img src={logo} className="h-[270] w-[450] m-auto p-2"></img>
+
+          <div className="font-inter font-thin text-6xl inline-block mt-4 ml-36">
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString("Gupta Eats")
+                  .pauseFor(1000)
+
+                  .start();
+              }}
+            />
+          </div>
+          <div className="font-inter font-thin text-6xl inline-block mt-4 ml-[160px]">
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString("Cravings Satisfied!")
+                  .pauseFor(1000)
+                  .deleteAll()
+                  .typeString("Hunger Conquered!")
+                  .start();
+              }}
+            />
+          </div>
         </div>
       </div>
       <div className="bg-pink-50 h-screen  w-1/2 -mt-2"></div>
